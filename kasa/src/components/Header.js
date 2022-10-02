d4956651b45bb41b34daade071fc4656b67bb4a6
Valentin Mamichel <valentin.mamichel.dijon@gmail.com>
2022-10-02 17:02:28 +0200

    kasa logo

--- kasa/src/components/Header.js
+++ kasa/src/components/Header.js
@@ -1,4 +1,13 @@
+import "../styles/Header.css";
+import logo from "../assets/logo.svg";
+
 function Header() {
-  return <h1>Kasa project</h1>;
+  const title = "Kasa";
+  return (
+    <div className="header">
+      <img src={logo} alt="logo kasa" className="kasa-logo" />
+      <h1 className="kasa-title">{title}</h1>;
+    </div>
+  );
 }
 export default Header;
